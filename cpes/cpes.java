1fa84be4c644ec7bbd076d36bf3a1d93222592a7
刘星原 <1796975445@qq.com>
2017-02-27 16:26:11 +0800

    cpes

---
+++ cpes/cpes.java
@@ -0,0 +1,24 @@
+package com.atguigu.cpes.bean;
+
+/**
+ *  @author 
+ */
+public class Cert {
+    private Integer id;
+    private String name;
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+    public Integer getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+}
